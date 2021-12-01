96c8d4489ecdd258526b9769b755ba7f4ca21c00
Chaitran77 <kiran@inteleyes.com>
2021-12-01 13:31:04 +0000

    Successfully writes to books.csv with correct formatting and repeats.

--- src/com/kiranthepro/Main.java
+++ src/com/kiranthepro/Main.java
@@ -1,6 +1,7 @@
 package com.kiranthepro;
 
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.LinkedHashMap;
@@ -8,13 +9,20 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) {
-        File libraryFile = initialiseFile("books.csv");
-        LinkedHashMap<String, String> bookInfo = getBookInfo();
+    private static String[] bookAttributes = {"title", "ISBN", "author", "genre", "year", "page count", "language", "cover type"};
 
-        String result = "";
-        bookInfo.forEach((key, value) -> System.out.println(key + ": " + value));
-        writeToFile(libraryFile, bookInfo);
+    public static void main(String[] args) {
+
+        File libraryFile = initialiseFile("books.csv");
+
+        boolean done = false;
+        while (!done) {
+            writeToFile(libraryFile, getBookInfo());
+            if (getInput("Would you like to add another file?").toLowerCase().contains("n")) {
+                done = true;
+            }
+        }
+
 
     }
 
@@ -23,6 +31,11 @@ public class Main {
 
         try {
             if (fileObj.createNewFile()) {
+                FileWriter writer = getFileWriter(fileObj);
+                assert writer != null;
+                writer.write(String.join(",", bookAttributes) + "\n");
+                writer.flush();
+                writer.close();
                 System.out.println("File " + fileObj.getName() + " created successfully.");
             } else {
                 System.out.println(fileObj.getName() + " already exists.");
@@ -37,7 +50,6 @@ public class Main {
 
     public static LinkedHashMap<String, String> getBookInfo() {
         LinkedHashMap<String, String> bookInfo = new LinkedHashMap<>();
-        String[] bookAttributes = {"title", "ISBN", "author", "genre", "year", "page count", "language", "cover type"};
 
         for (String attribute: bookAttributes) {
             bookInfo.put(attribute, getInput("Type in the " + attribute + " of the book:"));
@@ -47,15 +59,36 @@ public class Main {
     }
 
     public static void writeToFile(File file, LinkedHashMap<String, String> data) {
+        String[] values = new String[data.size()];
+        for (int i = 0; i < data.size(); i++) {
+            values[i] = (String) data.values().toArray()[i];
+        }
+        try {
+            FileWriter writer = getFileWriter(file);
+            if (writer != null) {
+                writer.write(String.join(",", values) + "\n");
+                writer.flush();
+                writer.close();
+            }
+        } catch (IOException e) {
+            System.out.println("Couldn't write to file - something went wrong :(");
+        }
 
+        System.out.println();
+    }
 
-//        System.out.println(String.join(",", dataArray));
+    public static FileWriter getFileWriter(File file) {
+        try {
+            return new FileWriter(file, true);
+        } catch (IOException e) {
+            return null;
+        }
     }
 
     public static String getInput(String prompt) {
         System.out.println(prompt);
         Scanner scanner = new Scanner(System.in);
-        return scanner.next();
+        return scanner.nextLine();
     }
 
 }
